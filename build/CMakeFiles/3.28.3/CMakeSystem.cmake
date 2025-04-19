854ba9b89a29ef1a4364557bc4f0d3ed1c5e1fc8
ZhuXiaobin <zzxiaobin09@qq.com>
2025-04-19 13:13:01 +0800

    my_MQTT_Client 2025.4.19

---
+++ build/CMakeFiles/3.28.3/CMakeSystem.cmake
@@ -0,0 +1,15 @@
+set(CMAKE_HOST_SYSTEM "Linux-5.18.19-051819-generic")
+set(CMAKE_HOST_SYSTEM_NAME "Linux")
+set(CMAKE_HOST_SYSTEM_VERSION "5.18.19-051819-generic")
+set(CMAKE_HOST_SYSTEM_PROCESSOR "x86_64")
+
+include("/home/zxb/linux/IMX6ULL/c_application/33_mqtt/mqtt_prj/cmake/arm-linux-setup.cmake")
+
+set(CMAKE_SYSTEM "Linux")
+set(CMAKE_SYSTEM_NAME "Linux")
+set(CMAKE_SYSTEM_VERSION "")
+set(CMAKE_SYSTEM_PROCESSOR "arm")
+
+set(CMAKE_CROSSCOMPILING "TRUE")
+
+set(CMAKE_SYSTEM_LOADED 1)
